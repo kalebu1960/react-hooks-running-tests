072850acdb812f67030dd374f80405be58cca11a
Your Name <caleb.muindi@student.moringaschool.com>
2025-07-11 23:50:34 +0300

    Completed React test lab: passing Header and Article tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import Article from './Article'
-import Header from './Header'
+import Article from './Article';
+import Header from './Header';
+import TestComponent from './TestComponent';
 
 function App() {
-  return <div className="App">
-    learn react!
-    <Header/>
-    <Article />
-  </div>;
+  return (
+    <div className="App">
+      <h1>Now</h1>
+      <Header />
+      <Article />
+      <TestComponent />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TestComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TestComponent() {
+  return (
+    <div>
+      <p>This is the Test Component!</p>
+    </div>
+  );
+}
+
+export default TestComponent;
